b03bf472c14511b9fab3fe9da28a949eca2cb86d
caishuxiang <1057756098@qq.com>
2022-07-18 14:40:19 +0800

    complete demo

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,110 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <button @click="handleExport">导出</button>
+    <table>
+       <thead>
+        <tr>
+          <th>序号</th>
+          <th>姓名</th>
+          <th>年龄</th>
+          <th>性别</th>
+        </tr>
+       </thead>
+       <tbody>
+         <tr v-for="item in tableData" :key="item.id">
+          <td>{{item.id}}</td>
+          <td>{{item.name}}</td>
+          <td>{{item.age}}</td>
+          <td>{{item.gender}}</td>
+         </tr>
+       </tbody>
+    </table>
   </div>
 </template>
 
 <script>
+import docxtemplater from 'docxtemplater'
+import PizZip from 'pizzip'
+import JSZipUtils from 'jszip-utils'
+import { saveAs } from 'file-saver'
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      tableData: [
+        {
+          id: 1,
+          name: "cc",
+          age: "22",
+          gender: "man",
+        },
+        {
+          id: 2,
+          name: "marry",
+          age: "25",
+          gender: "woman",
+        }
+      ]
+    }
+  },
+  methods: {
+    handleExport() {
+      console.log(this.tableData)
+      const data = {
+        titleDate: "2021-07-05 00:00 至 2022-07-18 00:00",
+        titleName: "测试表格",
+        table: this.tableData
+      }
+      this.exportDocx("Word/template.doc", data, "测试表格.doc");
+    },
+    exportDocx(tempDocxPath, data, fileName) {
+      JSZipUtils.getBinaryContent(tempDocxPath, function (error, content) {
+        if (error) {
+          throw error;
+        }
+        console.log("content==", content)
+        let zip = new PizZip(content);
+        let doc = new docxtemplater().loadZip(zip);
+        console.log("doc===", doc)
+        doc.setData(data);
+        try {
+          doc.render();
+        } catch (error) {
+          let e = {
+            message: error.message,
+            name: error.name,
+            stack: error.stack,
+            properties: error.properties,
+          };
+          console.log({
+            error: e
+          });
+          throw error;
+        }
+        let out = doc.getZip().generate({
+          type: "blob",
+          mimeType: "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+        });
+        console.log("out==", out)
+        saveAs(out, fileName);
+      });
+    },
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+table {
+    border-collapse: collapse;
+    border-spacing: 0;
+    margin-top:10px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+table td,
+table th {
+    text-align: center;
+    padding: 5px 10px;
+    border: 1px solid #ccc;
 }
 </style>
